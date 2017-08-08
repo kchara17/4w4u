84b9fc41c0a73ee629c295f36fb46c7a044824d1
Konstantinos <konstantinosc22@gmail.com>
2017-08-08 15:52:14 +0300

    test

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const store = require('./store')
 const app = express()
-var server = app.listen(5001);
+var server = app.listen(7555);
+
+/*server.listen(7555);
+
+server.on('connection', function(socket) {
+  console.log("A new connection was made by a client.");
+  socket.setTimeout(30 * 1000); 
+  // 30 second timeout. Change this as you see fit.
+})*/
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 /*app.post('/createUser', (req, res) => {
@@ -25,6 +34,9 @@ app.post('/login', (req, res) => {
     })
 })*/
 app.get('/place/:id', (req, res) => {
+	setTimeout(() => {
+    res.send('done');
+  }, 180000)
   store
     .getPlace({
 		id: req.params.id
@@ -34,15 +46,11 @@ app.get('/place/:id', (req, res) => {
       else res.sendStatus(401)
 	})
 })
+
 app.get('/place', (req, res) => {
   res.send("HEY!")
 })
-app.listen(7555, () => {
+server.timeout = 240000;  
+/*app.listen(7555, () => {
   console.log('Server running on http://localhost:7555')
-})
-
-app.on('connection', function(socket) {
-  console.log("A new connection was made by a client.");
-  socket.setTimeout(30 * 1000); 
-  // 30 second timeout. Change this as you see fit.
-})
+})*/
